ef0eec2a5425a0073bca4bfd04d787f2e9aea1b6
Dragonia428 <settineri.john@gmail.com>
2017-03-14 17:23:17 -0400

    Updated CPU Navigation

--- Assets/C#_Code/CPU_Behavior.cs
+++ Assets/C#_Code/CPU_Behavior.cs
@@ -1,31 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+[RequireComponent(typeof(NavMeshAgent))]
 public class CPU_Behavior : MonoBehaviour { 
+    
     NavMeshAgent navigation_path;
 
-    Vector3 previousPos;
-    Animator animate;
 
-    public float Forward_Distance;
+    public GameObject[] waypoints;
+
+  //  public float Forward_Distance;
    // AnimationState anims;
 
     public Transform target; 
     void Awake()
     {
          navigation_path = gameObject.GetComponent<NavMeshAgent>();
-        
+
+         
     }
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+	bool CheckIfAllWayPointsAreOccupied()
+    {
+        foreach(GameObject g in waypoints)
+        {
+            WaypointNavigation wp = g.GetComponent<WaypointNavigation>();
+            if (!wp.occupied) return false;
+        }
+        return false; 
+    }
 	// Update is called once per frame
 	void Update () {
-        if (gameObject.transform.position != target.position)
-            animate.SetFloat("Forward",Forward_Distance, 0.1f, Time.deltaTime);
+        if (waypoints.Length == 0)
+            navigation_path.SetDestination(gameObject.transform.position);
+        
+        navigation_path.SetDestination(target.position);
 
 
     }
+
 }
